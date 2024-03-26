9e40fa55d5ab10081e9a7e0cb33f35f8dd654a4d
caiovalverde20 <caiovalverde20@gmail.com>
2024-03-25 22:25:09 -0300

    ID categoria sequencial

--- src/main/java/Avalieaqui/category/CategoryController.java
+++ src/main/java/Avalieaqui/category/CategoryController.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/categories")
@@ -31,6 +34,15 @@ public class CategoryController {
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Categoria não encontrada.");
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+        long count = categoryRepository.count();
+        String id = String.valueOf(count + 1);
+        category.setId(id);
+        Category savedCategory = categoryRepository.save(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
     }
 
 }
