81d153e9016a5801db59e440ceff275acb2cb936
caiovalverde20 <caiovalverde20@gmail.com>
2024-04-02 17:56:30 -0300

    Arquivo base de testes

--- src/test/java/Avalieaqui/AvalieaquiApplicationTests.java
+++ src/test/java/Avalieaqui/AvalieaquiApplicationTests.java
@@ -2,9 +2,20 @@ package Avalieaqui;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootTest
 class AvalieaquiApplicationTests {
+
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(e -> registry.add(e.getKey(), e::getValue));
+	}
 
 	@Test
 	void contextLoads() {
