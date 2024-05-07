8c9433ac2debb3fff4ebc28f386e40eba535f5fa
igor-rib-souza <igorrs.rib@gmail.com>
2024-05-06 22:33:05 -0300

    setting up cors for delete

--- src/main/java/Avalieaqui/auth/SecurityConfig.java
+++ src/main/java/Avalieaqui/auth/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import com.google.api.client.json.gson.GsonFactory;
 
@@ -29,9 +32,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))
-                .authorizeRequests(requests -> requests.anyRequest().permitAll());
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "HEAD", "OPTIONS", "PUT", "PATCH", "DELETE"));
+                return configuration.applyPermitDefaultValues();
+            }))
+            .authorizeRequests(requests -> requests.anyRequest().permitAll());
         return http.build();
     }
 }
